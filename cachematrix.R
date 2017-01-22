eb48c7fe87c7a4870d99adb218daa00d2ee2851d
Pankaj-DataScience <gurukul.pankaj@gmail.com>
2017-01-22 15:00:56 -0600

    with corrections..

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## if TRUE cached inverse is returned  else its inverse will be calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
   if(!is.null(inv)) {
